47e636a43c1832049d2569798f310e97e891e0fe
Gavin Quirk <gavinquirk@gmail.com>
2019-05-10 16:51:53 -0700

    SignOutButton styled

--- src/components/SignOut/SignOutButton.js
+++ src/components/SignOut/SignOutButton.js
@@ -5,7 +5,7 @@ import './SignOutButton.css';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type='button' onClick={firebase.doSignOut}>
+  <button className='SignOutButton' type='button' onClick={firebase.doSignOut}>
     Sign Out
   </button>
 );
