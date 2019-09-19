1712c2967533e8063d29c4203870a0f413c0b686
Gavin Quirk <gavinquirk@gmail.com>
2019-09-19 12:02:11 -0700

    added title to iframe, fixed console warning

--- src/pages/SoundcloudPage/SoundcloudPage.js
+++ src/pages/SoundcloudPage/SoundcloudPage.js
@@ -8,12 +8,13 @@ export default function SoundcloudPage() {
     <div className='SoundcloudPage'>
       <h1 className='underline'>Soundcloud</h1>
       <iframe
+        title='Idyllwild Soundcloud'
         id='sc-widget'
         src='http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fusers%2F2&show_artwork=true&show_comments=false&color=ff00ff&show_playcount=false&liking=false'
         width='100%'
         height='100%'
         scrolling='no'
-        frameborder='no'
+        frameBorder='no'
       ></iframe>
     </div>
   );
